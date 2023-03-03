01f1be1aaf6d99bc0f90aa00bacc8cfcea2f0dd4
bobby9081 <124876400+bobby9081@users.noreply.github.com>
2023-03-03 23:24:30 +0530

    Add files via upload

---
+++ Assignment 4 Q1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+
+# In[17]:
+
+
+j = lambda a : a + 25
+print(j(10))
+j = lambda x, y : x * y
+
